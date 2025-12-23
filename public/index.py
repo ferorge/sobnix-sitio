edc26a8356ed8171f0dd3f73adb6d447f9d8a05c
ferorge <ferorge@texto-plano.xyz>
2025-12-23 19:50:19 -0300

    index.py | Cambio de nombre de fichero.

--- public/index.py
+++ public/index.py
@@ -4,5 +4,5 @@ print('Content-type: text/html\n\n')
 import os
 
 os.system("/usr/bin/bash -c 'cd .. && ./620.Creacion-sitio.sh'")
-with open("./sobnix.html") as f:
+with open("./Sobnix.html") as f:
     print(f.read())
