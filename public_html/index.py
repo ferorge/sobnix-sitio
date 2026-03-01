613a958c108243c49d0b524ea37aae25a968b440
ferorge <ferorge@texto-plano.xyz>
2026-03-01 09:26:47 -0300

    index.py | Cambio de la secuencia de escape.

--- public_html/index.py
+++ public_html/index.py
@@ -4,7 +4,7 @@ print('Content-type: text/html\n\n')
 import os
 
 os.system("/usr/games/fortune  /usr/share/games/fortunes/es/rms2 | /usr/bin/fold -s >  ../es/64-motd.md")
-os.system("/usr/bin/sed -i s/$/\ \ / ../es/64-motd.md")
+os.system("/usr/bin/sed -i s/$/'  '/ ../es/64-motd.md")
 oos.system("/usr/bin/bash -c 'cd .. && ./620.Creacion-sitio.sh'")
 with open("./Sobnix.html") as f:
     print(f.read())
