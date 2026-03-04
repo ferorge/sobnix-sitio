c570e751d790c2fdde9bd61960aa521e936c8556
ferorge <ferorge@texto-plano.xyz>
2026-03-04 15:10:02 -0300

    index.py | Error de tipeo.

--- public_html/index.py
+++ public_html/index.py
@@ -5,6 +5,6 @@ import os
 
 os.system("/usr/games/fortune  /usr/share/games/fortunes/es/rms2 | /usr/bin/fold -s >  ../es/68-motd.md")
 os.system("/usr/bin/sed -i s/$/'  '/ ../es/68-motd.md")
-oos.system("/usr/bin/bash -c 'cd .. && ./620.Creacion-sitio.sh'")
+os.system("/usr/bin/bash -c 'cd .. && ./620.Creacion-sitio.sh'")
 with open("./Sobnix.html") as f:
     print(f.read())
